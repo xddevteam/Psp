28e852d4cf24cf823f27351ad8b20da2e686e303
xddevteam <xddev65@gmail.com>
2025-03-10 15:28:10 +0700

    Upload lib/ad_helper.dart

---
+++ lib/ad_helper.dart
@@ -0,0 +1,5 @@
+class AdHelper {
+  static String get bannerAdUnitId => "ca-app-pub-5341881048632674/2897448469";
+  static String get interstitialAdUnitId => "ca-app-pub-5341881048632674/4481697726";
+  static String get appOpenAdUnitId => "ca-app-pub-5341881048632674/9768579770";
+}
