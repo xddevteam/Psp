7b3bf1d25455e24e580412f03ba4280f46c00746
xddevteam <xddev65@gmail.com>
2025-03-10 15:28:16 +0700

    Upload lib/main.dart

---
+++ lib/main.dart
@@ -0,0 +1,84 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+import 'banner_ad_widget.dart';
+import 'interstitial_ad_manager.dart';
+import 'app_open_ad_manager.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  MobileAds.instance.initialize();
+
+  // Inisialisasi dan muat App Open Ad
+  AppOpenAdManager().initialize();
+  InterstitialAdManager.loadAd();
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: WebViewScreen(),
+    );
+  }
+}
+
+class WebViewScreen extends StatefulWidget {
+  @override
+  _WebViewScreenState createState() => _WebViewScreenState();
+}
+
+class _WebViewScreenState extends State<WebViewScreen> {
+  late WebViewController _controller;
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setUserAgent("enzoXzodix")
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onPageStarted: (_) => setState(() => _isLoading = true),
+          onPageFinished: (_) {
+            setState(() => _isLoading = false);
+            InterstitialAdManager.showAd();
+          },
+          onWebResourceError: (error) async {
+            _controller.loadRequest(
+              Uri.dataFromString(
+                '<html><body><h1>Page Not Found</h1></body></html>',
+                mimeType: 'text/html',
+                encoding: Encoding.getByName('utf-8'),
+              ),
+            );
+          },
+        ),
+      )
+      ..loadRequest(Uri.parse('https://xdtoolts.xyz/'));
+
+    Future.delayed(Duration(seconds: 2), () {
+      AppOpenAdManager().showAdIfAvailable();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(toolbarHeight: 0),
+      body: Stack(
+        children: [
+          WebViewWidget(controller: _controller),
+          if (_isLoading) Center(child: CircularProgressIndicator()),
+        ],
+      ),
+      bottomNavigationBar: BannerAdWidget(),
+    );
+  }
+}
