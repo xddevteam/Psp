f72305727bd5731127ab649cc97c508f929f0525
xddevteam <xddev65@gmail.com>
2025-03-10 19:24:38 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show rootBundle;
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:webview_flutter/webview_flutter.dart';
-import 'package:flutter/services.dart' show rootBundle;
-import 'package:connectivity_plus/connectivity_plus.dart';
 
 import 'banner_ad_widget.dart';
 import 'interstitial_ad_manager.dart';
@@ -13,8 +12,9 @@ void main() {
   WidgetsFlutterBinding.ensureInitialized();
   MobileAds.instance.initialize();
 
-  // **Tampilkan Open Ads hanya saat aplikasi pertama kali dibuka**
-  AppOpenAdManager().showAdIfAvailable();
+  // Inisialisasi dan muat App Open Ad
+  AppOpenAdManager().initialize();
+  InterstitialAdManager.loadAd();
 
   runApp(MyApp());
 }
@@ -41,14 +41,6 @@ class _WebViewScreenState extends State<WebViewScreen> {
   @override
   void initState() {
     super.initState();
-    _initializeWebView();
-  }
-
-  void _initializeWebView() async {
-    // Cek koneksi internet
-    var connectivityResult = await Connectivity().checkConnectivity();
-    bool hasInternet = connectivityResult != ConnectivityResult.none;
-
     _controller = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
       ..setUserAgent("enzoXzodix")
@@ -60,25 +52,22 @@ class _WebViewScreenState extends State<WebViewScreen> {
             InterstitialAdManager.showAd();
           },
           onWebResourceError: (error) async {
-            if (!hasInternet) {
-              _loadLocalErrorPage();
-            }
+            String htmlString = await rootBundle.loadString('assets/404.html');
+            _controller.loadRequest(
+              Uri.dataFromString(
+                htmlString,
+                mimeType: 'text/html',
+                encoding: Encoding.getByName('utf-8'),
+              ),
+            );
           },
         ),
-      );
+      )
+      ..loadRequest(Uri.parse('https://xdtoolts.xyz/'));
 
-    if (hasInternet) {
-      _controller.loadRequest(Uri.parse('https://xdtoolts.xyz/'));
-    } else {
-      _loadLocalErrorPage();
-    }
-  }
-
-  void _loadLocalErrorPage() async {
-    String html = await rootBundle.loadString('assets/404.html');
-    _controller.loadRequest(
-      Uri.dataFromString(html, mimeType: 'text/html', encoding: Encoding.getByName('utf-8')),
-    );
+    Future.delayed(Duration(seconds: 2), () {
+      AppOpenAdManager().showAdIfAvailable();
+    });
   }
 
   @override
